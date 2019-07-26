11e5c8b7a1efa66b691e207b34eb4b3e44c29555
lewis <louieli2015m@outlook.com>
2019-07-26 10:43:07 +0800

    add new codes

--- hello_Python/hello_Python/hello_Python.py
+++ hello_Python/hello_Python/hello_Python.py
@@ -3,6 +3,19 @@
 # Test basic grammar
 print('Hello, Python!')
 print("Hello, Python!")
+print('Hello, Python')
+print("\"Yes,\" they said.")
+print('"Isn\'t," they said.')
+print('C:\some\name')
+print(r'C:\some\name')
+print(3 * 'un' + 'ium')
+print('Py' 'thon')
+# prevent End of line by adding a '\' at the end of the line.
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
 
 # Test function and bacis IO
 def hello_Python():  # print input string
@@ -11,5 +24,14 @@ def hello_Python():  # print input string
     print(read_str)
     return 0
 
-rev = hello_Python()
+
+# 
+def Fibonacci(n):
+    a, b = 0, 1
+    while a < n:
+        print(a)
+        a, b = b, a + b
+    return b
+
+rev = Fibonacci(input())
 print(rev)
